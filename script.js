3a53ec794f705470e2e827749a69887e59ffc765
hokrisztina <72090744+hokrisztina@users.noreply.github.com>
2020-10-04 11:17:12 +0200

    exercise 4 functions and for each loop

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('Krisztina H.');
+let n = 20;
+let Lettercount = n;
+for (let i = 1; i <= Lettercount; i++) {
+    console.log(i);
+   } // exercise 1 
+   
+
+
+   
+let greeting = (getGreetingTo, name) => {
+return getGreetingTo + name;          
+ }
+console.log(greeting ('Hello ','Mark', '!')); 
+// exercise 2
+
+
+let printValues = [array];
+    printValues.forEach((array) => {
+      console.log(array);
+    });
